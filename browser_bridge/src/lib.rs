bb07fb2ec4a4d75e5583055bd599be4a14e1e5f9
Nikita <116632007+Nikita55612@users.noreply.github.com>
2024-12-28 07:31:47 +0300

    Add files via upload

--- browser_bridge/src/lib.rs
+++ browser_bridge/src/lib.rs
@@ -1,1 +1,26 @@
+pub mod error;
 
+mod core;
+pub use core::{
+    DEFAULT_ARGS,
+    BrowserSession,
+    BrowserSessionConfig,
+    BrowserError,
+    BrowserTimings,
+    MyIP,
+    PageParam,
+    random_user_agent,
+};
+pub use core::extension;
+pub use chromiumoxide;
+
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[tokio::test]
+    async fn benchmark() {
+        assert_eq!(true, true);
+    }
+}
